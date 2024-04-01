758826a59f04fb49b03719c42b93ea9ca0b13816
Mingyi Liu <icecity96@outlook.com>
2024-04-01 08:10:30 +0800

    v0.3 example of generate javaDoc

--- src/ConscreteParkingField.java
+++ src/ConscreteParkingField.java
@@ -1,2 +1,39 @@
+import java.util.Map;
+
 public class ConscreteParkingField implements ParkingField{
+    /**
+     * @param plate
+     * @param width
+     * @param num
+     */
+    @Override
+    public void parking(String plate, int width, int num) {
+
+    }
+
+    /**
+     * @param plate
+     * @param width
+     */
+    @Override
+    public void parking(String plate, int width) {
+
+    }
+
+    /**
+     * @param plate
+     * @return
+     */
+    @Override
+    public double depart(String plate) {
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Map<Integer, String> status() {
+        return null;
+    }
 }
