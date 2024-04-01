43594e4b21261653bf9c9c6cdfb5abf8bd0571c8
Mingyi Liu <icecity96@outlook.com>
2024-04-01 08:09:13 +0800

    v0.2 ParkingField function collect

--- src/ParkingField.java
+++ src/ParkingField.java
@@ -1,2 +1,13 @@
+import java.util.Map;
+
 public interface ParkingField {
+    public static ParkingField create(){return null;} // 静态工厂方法，construction
+
+    public void parking(String plate, int width, int num); // mutator, 指定车位
+
+    public void parking(String plate, int width); // mutator, 不指定车位, 随机选择车位
+
+    public double depart(String plate); // mutator, 离场计费
+
+    public Map<Integer, String> status(); // observer
 }
